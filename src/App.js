72b53ebd9f00a537442c6158f973c429657f966b
Nikhil0722 <55701103+Nikhil0722@users.noreply.github.com>
2024-01-05 00:54:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+const [count, setCount] = useState(0);
+const add=()=>{
+  if(count==20){
+    setCount(count);
+  }else{
+    setCount(count+1);
+  }
+    
+}
+const sub=()=>{
+  if(count<=0){
+    setCount(count);
+  }else
+    {setCount(count-1);}
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Counter = {count}</h1>
+    <button onClick={add}>Increment</button>
+    <p></p>
+    <button onClick={sub}>Decrement</button>
+    </>
   );
 }
 
